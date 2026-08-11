ddc3f841e97c144ce11a8c0f7fcabf94af70e5d0
Abduk-kadir <ak8871639@gmail.com>
2026-08-11 18:46:38 +0530

    first commit

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,4 +1,4 @@
-const baseURL = "http://localhost:5000";
-//const baseURL='https://jtsc.dds-erp.com'
+//const baseURL = "http://localhost:5000";
+const baseURL='https://jtsc-cbse-webapp.dds-erp.com'
 
 export default baseURL;
